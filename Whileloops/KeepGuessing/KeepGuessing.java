2098b680ac2eb59323dffbfcd791fa5927bf5ece
angelc721 <86585097+angelc721@users.noreply.github.com>
2021-09-22 12:35:01 +0800

    upload exercise

--- Whileloops/KeepGuessing/KeepGuessing.java
+++ Whileloops/KeepGuessing/KeepGuessing.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 import java.util.Random;
 public class KeepGuessing
 {
-    public static void main (String[] args)
+	public static void main(String[] args)
     {
-        Scanner keyboard = new Scanner(System.in);
-        Random r = new Random();
-        int x=1+r.nextInt(10);
-        System.out.println("I'm thinking of a number from 1 to 10.");
-        int guess = keyboard.nextInt();
-        while (guess!=x)
+	    Scanner keyboard = new Scanner(System.in);
+	    Random r = new Random();
+	    int x=1+r.nextInt(10);
+	    System.out.println("I'm thinking of a number from 1 to 10.");
+	    int guess=0;
+	    while(guess!=x)
         {
-            System.out.print("Your guess: ");
-            guess = keyboard.nextInt();
-        }
-        if (guess == x)
+		    System.out.print("Your guess: ");
+		    guess = keyboard.nextInt();
+	    if(guess==x)
         {
-            System.out.println("That's right, you're a good guesser.");
-        }
+			System.out.print("That's right!  You're a good guesser.");
+	    }
         else
         {
-            System.out.println("That is incorrect. Guess again.");
-        }
-    }
+			System.out.println("That is incorrect. Guess again.");
+	    }
+	}
+  }
 }
