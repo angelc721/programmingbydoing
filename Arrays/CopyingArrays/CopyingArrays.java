12981aa4924b20b40a3d63de5a7398eb8c878ebe
angelc721 <86585097+angelc721@users.noreply.github.com>
2021-11-23 14:00:12 +0800

    upload exercise

--- Arrays/CopyingArrays/CopyingArrays.java
+++ Arrays/CopyingArrays/CopyingArrays.java
@@ -1,6 +1,7 @@
 //Arrays Copying Arrays 55 pts Angel Chuang
 import java.util.*;
-public class CopyingArrays{
+public class CopyingArrays
+{
 	public static void main(String[] args)
     {
 		Random r = new Random();
@@ -11,15 +12,16 @@ public class CopyingArrays{
 		for(int a=0; a<arr.length; a++)
         {
 			arr[a] = 1+r.nextInt(100);
+			arr[9] = -7; 
 			System.out.print(arr[a]+"  ");
 		}
 		arr1 = arr;
-		arr[arr.length - 1] = -7;
 		System.out.println();
 		
 		System.out.print("Array 2: ");
 		for(int b=0; b<arr1.length; b++)
         {
+			arr[9] = 1+r.nextInt(100); 
 			System.out.print(arr1[b]+"  ");
 		}	
 	}
